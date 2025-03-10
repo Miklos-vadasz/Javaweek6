ec755f92bcbd561af10b595d605fc155b661b475
Miklos-vadasz <mvadasz@stu.socsd.org>
2025-03-10 10:26:24 -0400

    frcyj

---
+++ Test_1.java
@@ -0,0 +1,32 @@
+/*Miklos Vadasz, March 10th 2025 Test_1*/
+import java.util.*;
+public class Test_1 {
+
+    public static void main(String[]args){
+
+        int concrete;
+        int feet;
+        
+        double volume;
+        double pie;
+        double r2;
+
+        r2 = 1.5 * 1.5;
+
+        pie = 3.14;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Concrete is 12.98 dollar");
+        System.out.print(volume = pie * r2 * 5);
+        System.out.print("Volume is: " + volume);
+
+
+
+
+
+
+
+    }
+
+ 
+}
---
+++ Test_2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Test_2 {
+
+    public static void main(String[]args){
+
+        int m;
+        int n;
+
+        Scanner input = new Scanner(System.in);
+
+        Scanner.out.print("ENter values for m and n, m is bigger than n: ");
+
+        System.out.print(m * m - n * n);
+        System.out.print(2 * m * n);
+        System.out.print(m * m + n * n);
+
+
+
+
+
+            
+        
+    }
+
+    
+}
